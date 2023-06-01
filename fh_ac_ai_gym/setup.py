1afbbfc0dcf7eb2786c8e6d75770c2e51604dbce
Nilusche Liyanaarachchi <niluschel@outlook.de>
2023-06-01 16:33:14 +0200

    fix:gym version

--- fh_ac_ai_gym/setup.py
+++ fh_ac_ai_gym/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='fh_ac_ai_gym',
       version='0.0.1',
-      install_requires=['gym','numpy', 'sympy']
+      install_requires=['gym==0.23.1','numpy', 'sympy']
       )
